3966468eb6b3e9c33291e865dfc18280d0a57513
Gamilliam <94926518+Gamilliam@users.noreply.github.com>
2023-10-16 13:31:58 +0000

    Inventory entity updated

--- src/inventory/entities/inventory.entity.ts
+++ src/inventory/entities/inventory.entity.ts
@@ -1,1 +1,38 @@
-export class Inventory {}
+import { Item } from "src/item/entities/item.entity";
+import { Personnage } from "src/personnage/entities/personnage.entity";
+import { Column, CreateDateColumn, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class Inventory {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        type: 'int',
+        unique: false,
+        default: 0,
+    })
+    gold: number;
+
+    // --------------TIMESTAMPS
+    @CreateDateColumn({
+        type: 'timestamp',
+        default: () => 'CURRENT_TIMESTAMP(6)',
+    })
+    created_at: Date;
+
+    @UpdateDateColumn({
+        type: 'timestamp',
+        default: () => 'CURRENT_TIMESTAMP(6)',
+        onUpdate: 'CURRENT_TIMESTAMP(6)',
+    })
+    updated_at: Date;
+
+    // --------------RELATIONS
+
+    @ManyToMany(() => Item, (item) => item.inventories)
+    @JoinTable()
+    items: Item[];
+
+    @ManyToOne(() => Personnage, (personnage) => personnage.inventories)
+    personnage : Personnage;
+}
